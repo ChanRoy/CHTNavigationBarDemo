c335c71c9ba2c6b71da46b1bff1ae8832fa9bc0d
ChanRoy <chen_ht89@163.com>
2017-02-17 10:46:19 +0800

    demo finished

--- CHTNavigationBarDemo/ViewController.m
+++ CHTNavigationBarDemo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#define SYSTEM_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
+
 @interface ViewController ()
 
 @end
@@ -18,99 +20,55 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor grayColor];
-//
-//    [self cht_getBackView:self.navigationController.navigationBar color:[UIColor orangeColor]];
     
+    //隐藏UINavigationBar下方的直线
     [self findHairlineImageViewUnder:self.navigationController.navigationBar].hidden = YES;
     
-    [self getBackView:self.navigationController.navigationBar Color:[UIColor clearColor]];
-    
+    //改变UINavigationBar的颜色
+    [self cht_getBackView:self.navigationController.navigationBar color:[UIColor orangeColor]];
+
 }
 
 - (void)cht_getBackView:(UIView *)view color:(UIColor *)color{
     
     [self.navigationController setNavigationBarHidden:NO animated:YES];
     
-#if ( defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 100000 )
-    //iOS10⬆️
-    
-    if ([view isKindOfClass:NSClassFromString(@"_UIBarBackground")] || [view isKindOfClass:NSClassFromString(@"UIVisualEffectView")]) {
-        view.backgroundColor = color;
+    if (SYSTEM_VERSION < 10) {
         
-        if ([color isEqual:[UIColor clearColor]]) {
+        // <iOS10
+        if ([view isKindOfClass:NSClassFromString(@"_UINavigationBarBackground")]) {
             
+            view.backgroundColor = color;
+        }else if ([view isKindOfClass:NSClassFromString(@"_UIBackdropView")]){
+            
+            //将_UINavigationBarBackground上面的遮罩层隐藏
             view.hidden = YES;
         }
-    }
-    
-    for (UIView *subView in view.subviews) {
-        
-        [self cht_getBackView:subView color:color];
-    }
-    self.navigationController.navigationBar.barTintColor = color;
-    
-#else
-    
-    //iOS10⬇️
-    if ([view isKindOfClass:NSClassFromString(@"_UINavigationBarBackground")]) {
-        
-        view.backgroundColor = color;
-    }else if ([view isKindOfClass:NSClassFromString(@"_UIBackdropView")]){
-        
-        //将_UINavigationBarBackground上面的遮罩层隐藏
-        view.hidden = YES;
-    }
-    for (UIView *subView in view.subviews) {
-        
-        [self cht_getBackView:subView color:color];
-    }
-    
-#endif
-}
-
-- (void)getBackView:(UIView *)View Color:(UIColor *)color{
-#if 1
-    [self.navigationController setNavigationBarHidden:NO animated:YES];
-    //||[View isKindOfClass:NSClassFromString(@"_UIBarBackground")]
-    //[View isKindOfClass:NSClassFromString(@"_UINavigationBarBackground")]
-    if ([View isKindOfClass:NSClassFromString(@"_UINavigationBarBackground")] || [View isKindOfClass:NSClassFromString(@"_UIBarBackground")] ) {
-        //背景颜色设置
+        for (UIView *subView in view.subviews) {
             
-        View.backgroundColor = color;
-
-    }else if ([View isKindOfClass:NSClassFromString(@"_UIBackdropView")]){
-        
-        //将_UINavigationBarBackground上面的遮罩层隐藏
-        View.hidden = YES;
-    }
-#if 1
-    //iOS10 navigationbar 有点不一样，先这样设置
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 10) {
-        
-        if ([View isKindOfClass:NSClassFromString(@"UIVisualEffectView")]){
-            
-            View.backgroundColor = color;
-            
-            if ([color isEqual:[UIColor clearColor]]) {
-                View.hidden = YES;
-            }
-            
+            [self cht_getBackView:subView color:color];
         }
     }
-#endif
-    
-    for (UIView *view in View.subviews) {
-        
-        [self getBackView:view Color:color];       //递归遍历NavBar视图
-        
+
+#ifdef __IPHONE_10_0
+    else{
+        // >=iOS10
+        if ([view isKindOfClass:NSClassFromString(@"_UIBarBackground")] ||
+            [view isKindOfClass:NSClassFromString(@"UIVisualEffectView")]) {
+            view.backgroundColor = color;
+            
+            if ([color isEqual:[UIColor clearColor]]) {
+                
+                view.hidden = YES;
+            }
+        }
+        for (UIView *subView in view.subviews) {
+            
+            [self cht_getBackView:subView color:color];
+        }
+        self.navigationController.navigationBar.barTintColor = color;
     }
 #endif
-    //iOS10有点坑爹，先这样设置
-    if([[[UIDevice currentDevice] systemVersion] floatValue] >= 10){
-        
-        [self.navigationController.navigationBar setBarTintColor:color];
-    }
-    
 }
 
 //递归寻找UINavigationBar下方的直线
